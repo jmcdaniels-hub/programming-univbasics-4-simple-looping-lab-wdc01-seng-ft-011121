806a233d6ece502cb04a69195bf73d98eea0ee63
Jade McDaniels <jademcdaniels81@gmail.com>
2020-12-26 01:07:46 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -26,7 +26,11 @@ end
 
 
 def return_string_array(array = ["5", "4", "3", "2", "1"]
-  print array
+  count = 0 
+  while count < array.length do
+    array[count].to_s
+    count +=1 
+  end  
 end 
 
 
